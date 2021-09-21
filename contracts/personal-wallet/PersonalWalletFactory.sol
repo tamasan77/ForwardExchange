da9e41ab1735966aff7432a86fe518b08a898d6e
Tamas An <tamasan77@gmail.com>
2021-09-21 13:12:35 -0500

    added PersonalWalletFactory.sol

--- contracts/personal-wallet/PersonalWalletFactory.sol
+++ contracts/personal-wallet/PersonalWalletFactory.sol
@@ -6,11 +6,18 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 //import "@openzeppelin/contracts/security/Pausable.sol";
 import "./PersonalWallet.sol";
 
+/// @title Personal wallet factory
+/// @author Tamas An
+/// @notice Factory that creates and holds addresses of personal wallets.
 contract PersonalWalletFactory {
     address[] public personalWallets;
 
     event WalletCreated(address walletOwner);
 
+    /// @notice Creates Personal Wallet
+    /// @param walletOwner Address of the owner of the wallet
+    /// @param walletName Name of the wallet
+    /// @return personalWallet_ Address of the newly created wallet.
     function createPersonalWallet(address walletOwner, string memory walletName) 
         external 
         returns (address personalWallet_) 
